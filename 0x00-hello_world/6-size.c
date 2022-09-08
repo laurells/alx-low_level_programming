8bce5f390ff6676d8c61f01753e5464760e934c9
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2022-09-08 15:14:56 -0700

    code is executable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - prints the size of various types
  * Return: 0 if exited properly.
@@ -11,6 +11,5 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-
 	return (0);
 }
