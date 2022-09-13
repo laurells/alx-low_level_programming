01c582ad2c578ce1aae74da75760cb3f1417e34e
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2022-09-13 11:40:38 -0700

    The 5-sign.c program has been edited

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,13 +9,13 @@
  */
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
 	} else if (n == 0)
 	{
-		_putchar(48);
+		_putchar(98);
 		return (0);
 	} else if (n < 0)
 	{
