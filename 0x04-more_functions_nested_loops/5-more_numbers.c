dc1996dde7225a3e59f8d5ace256732f8b691893
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2022-09-16 13:57:06 -0700

    C functions and nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -17,9 +17,8 @@ void more_numbers(void)
 			{
 				_putchar((z / 10) + '0');
 			}
-			_putchar((z % 10) + '0');	
+			_putchar((z % 10) + '0');
 		}
 		_putchar('\n');
 	}
 }
-
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ int main(void)
 			printf("Fizz ");
 		else if (i == 100)
 			printf("Buzz");
-		else if ((i % 5) == 0 && (i % 3) !=0)
+		else if ((i % 5) == 0 && (i % 3) != 0)
 			printf("Buzz ");
 		else if ((i % 3) == 0 && (i % 3) == 0)
 			printf("FizzBuzz ");
