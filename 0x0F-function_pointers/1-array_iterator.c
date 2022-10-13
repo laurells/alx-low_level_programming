63d0667c35c095ebb56f547759eb6b374789fd7d
Laurels Echichinwo <laurels.echichinwo@stu.cu.edu.ng>
2022-10-13 12:48:55 -0700

    My second commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,13 +8,15 @@
  * @size: size of the array
  * @action: pointer to function used
  */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
 	if (!array || !action)
 		return;
-
 	for (i = 0; i < size; i++)
+	{
 		action(array[i]);
+	}
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,23 +5,21 @@
  * @array: array to search in
  * @size: size of the array
  * @cmp: pointer to the comparing function
- *
  * Return: index of the first element for which
  * the cmp function does not return 0, or -1 if no match is found
  * or size is negative
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int x;
 
-	if (array && cmp)
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);	
+	for (x = 0; x < size; x++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i]) != 0)
-				return (i);
-		}
+		if (cmp(array[x]))
+			return (x);
 	}
-
 	return (-1);
 }
